be6f39e4ff92e4a9cc67cd8dfde1d03da8d6e8c1
Nathan <93925029+Nath9666@users.noreply.github.com>
2023-10-11 08:41:30 +0200

    lab4

---
+++ Lab4/vue-oauth-microsoft-graph/src/components/asyncButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <base-button
+    :disabled="isPending"
+    :color="color"
+    @click.stop.prevent="handleClick"
+  >
+    <span v-if="isPending">Loading ...</span>
+    <slot />
+  </base-button>
+</template>
+
+<script>
+import BaseButton from "./baseButton.vue";
+
+export default {
+  name: "AsyncButton",
+  components: { BaseButton },
+  inheritAttrs: false,
+
+  props: {
+    color: {
+      type: String,
+      default: "primary",
+    },
+    initialTimer: {
+      type: Number,
+      default: 2000,
+    },
+  },
+
+  data() {
+    return {
+      isPending: false,
+      timer: this.initialTimer,
+    };
+  },
+
+  methods: {
+    handleClick() {
+      console.log("Async button clicked", this.timer);
+
+      // Create a new promise with the current timer value
+      const asyncOperation = new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, this.timer);
+      });
+
+      // Update timer for the next click
+      this.timer += 1000;
+
+      // Toggle isPending when promise resolves
+      asyncOperation.then(() => {
+        this.isPending = !this.isPending;
+      });
+
+      // Toggle isPending immediately
+      this.isPending = !this.isPending;
+    },
+  },
+};
+</script>
---
+++ Lab4/vue-oauth-microsoft-graph/src/components/baseContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <BaseHeader />
+    <slot></slot>
+    <BaseFooter />
+  </div>
+</template>
+
+<script>
+import BaseHeader from "./baseHeader.vue";
+import BaseFooter from "./baseFooter.vue";
+
+export default {
+  name: "BaseContent",
+  components: {
+    BaseHeader,
+    BaseFooter,
+  },
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
